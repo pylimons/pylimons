5e9339b86aae5c8bbf77d3ce27ecd2c566115135
pylimons <pylimons@gmail.com>
2022-11-16 16:08:53 +0900

    update elements/element.py

--- elements/element.py
+++ elements/element.py
@@ -5,14 +5,23 @@ import numpy as np
 class Element(object):
     types = ["drift", "sbend", "rbend", "quadrupole", "sextupole", "octupole", "solenoid", "rfcavity", "marker"]
     properties = ["name", "type", "length", "strength", "angle", "phi", "k1", "ks", "aperture"]
+    aperture_types = ["rectangular", "circular", "elliptical"]
     
-    def __init__(self, name, elmtype, length=0, strength=0):
+    def __init__(self, name, elmtype, length=0, strength=0, aperture=[]):
         if elmtype.lower() not in self.__class__.types:
             print (elmtype.lower())
             print ("The element type is not in the type list", file=sys.stderr)
             return
-        
+
         self.element_properties = {}
+        if len(aperture) > 1:
+            if aperture[0].lower() not in self.__class__.aperture_types:
+                print ("The aperture type is not properly defined")
+            else:
+                self.element_properties["aperture"] = aperture
+        else:
+            self.element_properties["aperture"] = "Not defined"
+
         self.element_properties["name"] = name.lower()
         self.element_properties["type"] = elmtype.lower()
         self.element_properties["length"] = length
@@ -33,11 +42,25 @@ class Element(object):
         else:
             print ("The property {} is not defined".format(prop.lower()))
 
+    def set_aperture_property(self, **param):
+        for k, v in param.items():
+            if k.lower() in self.__class__.aperture_properties:
+                self.aperture_attributes[k.lower()] = v
+            else:
+                print ("Not a proper aperture property", file=sys.stderr)
+
+    def get_aperture_property(self, prop):
+        if prop.lower() in self.__class__.aperture_properties:
+            print ("The aprture's {} is {}".format(prop.lower(), self.aperture_attributes[prop.lower()]))
+        else:
+            print ("The property {} is not defined".format(prop.lower()))
+
     def print_properties(self):
         print ("element name     :", self.element_properties["name"])
         print ("element type     :", self.element_properties["type"])
         print ("element length   :", self.element_properties["length"])
         print ("element strength :", self.element_properties["strength"])
+        print ("element aperture :", self.element_properties["aperture"])
         
     def element_copy(self, new_element_name):
         new_element = copy.deepcopy(self)
