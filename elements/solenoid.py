9574d3e30e9786b1284f4ce3975200e0f46ccbc6
pylimons <pylimons@gmail.com>
2022-11-16 14:51:38 +0900

    add solenoid element

---
+++ elements/solenoid.py
@@ -0,0 +1,49 @@
+import sys
+import numpy as np
+from .element import Element
+
+_x, _xp, _y, _yp, _tau, _dp = range(6)
+
+class Solenoid(Element):
+    def __init__(self, name, elmtype="drift", length=0, strength=0):
+        super().__init__(name, elmtype, length, strength)
+        self.element_properties["ks"] = strength
+        
+    def get_transfer_matrix(self, dim=4):
+        L = self.element_properties["length"]
+        ks = self.element_properties["ks"]
+        
+        mat = np.zeros([dim, dim], dtype=float)
+        
+        ksL = ks * L
+        
+        cosksL = np.cos(ksL)
+        sin2ksL = np.sin(2 * ksL)
+        
+        mat[_x, _x] = mat[_xp, _xp] = mat[_y, _y] = mat[_yp, _yp] = cosksL * cosksL
+        mat[_x, _xp] = mat[_y, _yp] = sin2ksL / (2 * ks)
+        mat[_xp, _x] = mat[_yp, _y] = - sin2ksL * ks / 2
+        mat[_x, _y] = mat[_xp, _yp] = sin2ksL / 2
+        mat[_y, _x] = mat[_yp, _xp] = - sin2ksL / 2
+        mat[_x, _yp] = sin2ksL * sin2ksL / ks
+        mat[_yp, _x] = sin2ksL * sin2ksL * ks
+        mat[_xp, _y] = - sin2ksL * sin2ksL * ks
+        mat[_y, _xp] = - sin2ksL / ks
+        
+        #TODO
+        #if dim == 6:
+        #    mat[4, 4] = mat[5,5] = 1
+        #    mat[4, 5] = self.length / (beta0 * gamma0)**2
+        
+        return mat
+        
+    def propagate(self, particles):
+        transfer_map = self.get_transfer_matrix(particles.shape[0])
+        
+        new_particles = transfer_map @ particles
+        
+        if new_particles.shape != particles.shape:
+            print ("something is wrong in propgation", file=sys.stderr)
+            
+        return new_particles
+        
