44e54a5b6f284cf9d02ec3dddaa42d97c612c984
pylimons <pylimons@gmail.com>
2022-11-16 14:51:50 +0900

    add marker element

---
+++ elements/marker.py
@@ -0,0 +1,13 @@
+import sys
+import numpy as np
+from .element import Element
+
+_x, _xp, _y, _yp, _tau, _dp = range(6)
+
+class Marker(Element):
+    def __init__(self, name, elmtype="marker"):
+        super().__init__(name, elmtype)
+        
+    def propagate(self, particles):
+        return particles
+        
