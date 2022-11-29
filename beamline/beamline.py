a59e58f2e6085d4eb9554c37d4c1cedbe6df7c26
pylimons <pylimons@gmail.com>
2022-11-29 15:39:53 +0900

    add logger in beamline propagation

--- beamline/beamline.py
+++ beamline/beamline.py
@@ -19,8 +19,21 @@ class Beamline(object):
                 return
             
     def propagate_beamline(self, bunch):
+        index = 0
+        length = 0
         for element in self.lattice:
             element.propagate(bunch)
+            element_name = element.element_properties["name"]
+            element_type = element.element_properties["type"]
+            element_length = element.element_properties["length"]
+            length += element_length
+            
+            emit_x, emit_y = bunch.get_emittance()
+            
+            print ("%10d %10s %10s %10.5f %10.5f %10d/%10d %10.5f %10.5f" % (index, element_name, element_type, element_length, \
+                                                   length, bunch.get_num_particles(), bunch.get_original_num_particles(), \
+                                                                             emit_x*1e6, emit_y*1e6))
+            index += 1
             
     def propagate_sliced_beamline(self, bunch):
         for element in self.sliced_lattice:
